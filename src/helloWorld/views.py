8a9d4071c4a3452e4a25ec95f8e6f7e579c4dbad
mb665616 <milad.bonakdar@broadcom.com>
2019-01-31 00:15:34 -0800

    v2 code

--- src/helloWorld/views.py
+++ src/helloWorld/views.py
@@ -5,10 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def Hello(request):
     if request.method == 'GET':
-        return HttpResponse('Hello World!')
+        return HttpResponse('Hello World V2!')
     elif request.method == 'POST':
         name = request.POST.get("name")
         if name:
-            return HttpResponse("Hello " + name + " World!")
+            return HttpResponse("Hello " + name + " World V2!")
         else:
             return HttpResponse(status=400)
