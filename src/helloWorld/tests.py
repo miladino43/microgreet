6b3001dd1d4167924fedd98023835a08a79d2c53
mb665616 <milad.bonakdar@broadcom.com>
2019-01-30 22:21:57 -0800

    add some basic tests

--- src/helloWorld/tests.py
+++ src/helloWorld/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.http import HttpRequest
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+from . import views
+
+
+class HomePageTests(SimpleTestCase):
+
+    def test_get(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_post_success(self):
+        response = self.client.post(reverse('hello'), {'name': 'milad'})
+        self.assertEquals(response.status_code, 200)
+
+    def test_post_fail(self):
+        response = self.client.post(reverse('hello'))
+        self.assertEquals(response.status_code, 400)
